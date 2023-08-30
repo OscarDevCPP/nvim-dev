10aabba86397afad11f17a65cf665b39584a3f83
OscarDevCPP <oscarchh0799@gmail.com>
2023-08-29 23:19:12 -0500

    FEAT Instala y configura packer manager
    Se instalo lazy.nvim para el manejo de plugins
    
    issue #1

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
-require('settings')
+-- Cargando modulo de configuraciónes generales para nvim 
+require('oscar-dev.settings')
+
+-- Cargar lazy.nvim e instalarno en caso no este instalado
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({})
 
 
