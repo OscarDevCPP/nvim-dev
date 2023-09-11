d2e550d84ea8a874a6ecb92257080908a668eebd
OscarDevCPP <oscarchh0799@gmail.com>
2023-09-11 01:38:42 -0500

    FEAT configura entorno de desarrollo java jdtls
    issue #

--- lua/oscar-dev/plugins/telescope/init.lua
+++ lua/oscar-dev/plugins/telescope/init.lua
@@ -18,12 +18,34 @@ return {
           theme = "ivy"
         }
       },
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+            -- even more opts
+          }
+
+          -- pseudo code / specification for writing custom displays, like the one
+          -- for "codeactions"
+          -- specific_opts = {
+          --   [kind] = {
+          --     make_indexed = function(items) -> indexed_items, width,
+          --     make_displayer = function(widths) -> displayer
+          --     make_display = function(displayer) -> function(e)
+          --     make_ordinal = function(e) -> string
+          --   },
+          --   -- for example to disable the custom builtin "codeactions" display
+          --      do the following
+          --   codeactions = false,
+          -- }
+        }
+      }
     }
 
     telescope.load_extension('fzf')
     telescope.load_extension('notify')
     telescope.load_extension('projects')
     telescope.load_extension('dap')
+    telescope.load_extension("ui-select")
 
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
